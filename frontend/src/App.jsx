2e938f382f5dde65bc47e0ffdc4f3d9196b75cc9
yashica1511 <mailtoyashi04@gmail.com>
2024-10-24 14:25:09 +0530

    Corrected code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import QuestionPanel from './components/QuestionPanel';
 
 
 function App() {
   return (
+    <Router>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/question" element={<QuestionPanel/>}/>
     </Routes>
+    </Router>
   );
 }
 
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,9 +3,9 @@ import CodeCompiler from "./CodeCompiler";
 
 function Home() {
   return (
-    <div className="min-h-screen bg-blue-600 text-white flex flex-col">
+    <div className="min-h-screen bg-black text-white flex flex-col">
       <div className="flex-grow flex flex-col lg:flex-row">
-        <div className="lg:w-1/2 w-full p-6 border-r border-blue-300 bg-white text-black rounded-xl shadow-lg mb-6 lg:mb-0">
+        <div className="lg:w-1/2 w-full p-6 border-r border-black bg-white text-black rounded-xl shadow-lg mb-6 lg:mb-0">
           <QuestionPanel />
         </div>
         <div className="lg:w-1/2 w-full p-6 bg-white text-black rounded-xl shadow-lg">
